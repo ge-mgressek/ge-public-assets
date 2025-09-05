3fd34f38ea654d5a5e2f4c974360b98bb9c04305
ge-mgressek <matt@globe-eco.com>
2025-09-05 19:14:01 -0400

    Prep for vite build system

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+  plugins: [
+    ViteImageOptimizer({
+      /* pass your config here */
+      // Example: optimize JPGs to 80% quality
+      jpg: {
+        quality: 80,
+      },
+      png: {
+        quality: 80,
+      },
+    }),
+  ],
+});
