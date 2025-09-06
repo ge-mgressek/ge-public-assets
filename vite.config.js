c8a3713d1ee834bc260d6539dbe9b0e3c0c768f5
ge-mgressek <matt@globe-eco.com>
2025-09-05 20:49:10 -0400

    Updated Vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,10 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: 'src',
+  build: {
+    outDir: '../dist' // <-- Add this build object
+  },
   plugins: [
-    ViteImageOptimizer({
-      /* pass your config here */
-      // Example: optimize JPGs to 80% quality
-      jpg: {
-        quality: 80,
-      },
-      png: {
-        quality: 80,
-      },
-    }),
+    ViteImageOptimizer(),
   ],
 });
