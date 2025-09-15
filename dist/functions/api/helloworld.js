d2734c680b15f06f7a25dc074601db11ce31aab7
ge-mgressek <matt@globe-eco.com>
2025-09-15 16:15:34 -0400

    Remove dist folder from repository

--- dist/functions/api/helloworld.js
+++
@@ -1,9 +0,0 @@
-// File: functions/api/hello.js
-
-export function onRequest(context) {
-  // context object contains request, env, etc.
-  
-  return new Response("Hello, World!", {
-    headers: { 'Content-Type': 'text/plain' },
-  });
-}
